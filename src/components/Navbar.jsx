d4b5cfa8e66b381b1a3a2f9b8339494a149baea4
Faheem-Rafeeq <imfaheemrafeeq78@gmail.com>
2025-08-15 03:44:16 +0500

    navbar almost complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       {/* Logo */}
       <Link to="/" onClick={() => setIsMenuOpen(false)}>
         <img
-          className="w-[80px] md:w-[97px] h-[70px] md:h-[70px] mt-[7px] cursor-pointer"
+          className="w-[80px] md:w-[87px] h-[70px] md:h-[70px] mt-[7px] cursor-pointer"
           src={fmunblack}
           alt="FMUN Logo"
         />
@@ -36,32 +36,32 @@ const Navbar = () => {
       <div className="hidden md:block">
         <ul className="flex flex-row space-x-4 lg:space-x-6">
           <li>
-            <Link to="/" className="text-white font-bold cursor-pointer font-sans text-lg lg:text-xl hover:text-gray-300 transition">
+            <Link to="/" className="text-black cursor-pointer  font-extralight  text-lg lg:text-xl hover:text-gray-300 transition">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className="text-white font-bold cursor-pointer font-sans text-lg lg:text-xl hover:text-gray-300 transition">
+            <Link to="/about" className="text-black cursor-pointer font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
               About us
             </Link>
           </li>
           <li>
-            <Link to="/committee" className="text-white font-bold cursor-pointer font-sans text-lg lg:text-xl hover:text-gray-300 transition">
-              Committe
+            <Link to="/committee" className="text-black  cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
+              Committee
             </Link>
           </li>
           <li>
-            <Link to="/schedule" className="text-white font-bold cursor-pointer font-sans text-lg lg:text-xl hover:text-gray-300 transition">
+            <Link to="/schedule" className="text-black  cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
               Schedule
             </Link>
           </li>
           <li>
-            <Link to="/announcements" className="text-white font-bold cursor-pointer font-sans text-lg lg:text-xl hover:text-gray-300 transition">
+            <Link to="/announcements" className="text-black  cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
               Announcements
             </Link>
           </li>
           <li>
-            <Link to="/register" className="text-white font-bold cursor-pointer font-sans text-lg lg:text-xl hover:text-gray-300 transition">
+            <Link to="/register" className="text-black cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
               Register
             </Link>
 
@@ -71,48 +71,48 @@ const Navbar = () => {
 
       {/* Button - Desktop */}
       <div className="hidden md:block flex items-center space-x-4">
-        <Link to="/login" className="gap-1 flex bg-white text-black font-medium px-4 lg:px-5 py-2 cursor-pointer hover:bg-gray-200 transition items-center">
-          Login <span className='text-xl'><GoArrowUpRight /></span>
+        <Link to="/login" className="gap-1 flex bg-white text-black  font-extralight px-4 lg:px-5 py-2 cursor-pointer hover:bg-gray-200 transition items-center">
+          Login <span className='text-xl mt-1'><GoArrowUpRight /></span>
         </Link>
       </div>
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="md:hidden absolute top-[90px] left-0 w-full bg-blue-300 z-50 py-4 px-4">
+        <div className="md:hidden absolute top-[90px] left-0 w-full bg-blue-300 z-50 py-4 px-4 ">
           <ul className="flex flex-col space-y-4">
             <li>
-              <Link to="/" className=" text-black  cursor-pointer font-sans text-xl hover:text-gray-300 transition py-2 block" onClick={() => setIsMenuOpen(false)}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className=" text-black cursor-pointer font-sans text-xl hover:text-gray-300 transition py-2 block" onClick={() => setIsMenuOpen(false)}>
-                About us
-              </Link>
-            </li>
-            <li>
-              <Link to="/committee" className=" text-black cursor-pointer font-sans text-xl hover:text-gray-300 transition py-2 block" onClick={() => setIsMenuOpen(false)}>
-                Committee
-              </Link>
-            </li>
-            <li>
-              <Link to="/schedule" className=" text-black cursor-pointer font-sans text-xl hover:text-gray-300 transition py-2 block" onClick={() => setIsMenuOpen(false)}>
-                Schedule
-              </Link>
-            </li>
-            <li>
-              <Link to="/announcements" className=" text-black cursor-pointer font-sans text-xl hover:text-gray-300 transition py-2 block" onClick={() => setIsMenuOpen(false)}>
-                Announcements
-              </Link>
-            </li>
+            <Link to="/" className="text-black cursor-pointer  font-extralight  text-lg lg:text-xl hover:text-gray-300 transition">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-black cursor-pointer font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
+              About us
+            </Link>
+          </li>
+          <li>
+            <Link to="/committee" className="text-black  cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
+              Committee
+            </Link>
+          </li>
+          <li>
+            <Link to="/schedule" className="text-black  cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
+              Schedule
+            </Link>
+          </li>
+          <li>
+            <Link to="/announcements" className="text-black  cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
+              Announcements
+            </Link>
+          </li>
+          <li>
+            <Link to="/register" className="text-black cursor-pointer  font-extralight text-lg lg:text-xl hover:text-gray-300 transition">
+              Register
+            </Link>
 
-            <li>
-              <Link to="/register" className="text-black cursor-pointer font-sans text-xl hover:text-gray-300 transition py-2 block" onClick={() => setIsMenuOpen(false)}>
-                Register
-              </Link>
-            </li>
+          </li>
             <li className="flex items-center space-x-4">
-                <Link to="/login" className="gap-1 flex bg-white text-black font-medium px-4 lg:px-5 py-2 cursor-pointer hover:bg-gray-200 transition items-center">
+                <Link to="/login" className="gap-1 flex bg-white text-black font-extralight px-4 lg:px-5 py-2 cursor-pointer hover:bg-gray-200 transition items-center">
                   Login <span className='text-xl mt-1'><GoArrowUpRight /></span>
                 </Link>
             </li>
